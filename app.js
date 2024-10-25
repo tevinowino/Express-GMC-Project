fd6794790da08c84dc3f7f27290267c4d66f825c
tevinowino <tevinowino65@gmail.com>
2024-10-25 11:03:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('home', { title: 'Barbershop' });
+});
+
+app.get('/our-services', (req, res) => {
+  res.render('our-services', { title: 'Barbershop' });
+});
+
+app.get('/contact-us', (req, res) => {
+  res.render('contact-us', { title: 'Barbershop' });
+});
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Start the server
+app.listen(5000, () => {
+  console.log('Server started on port 5000');
+});
+
+module.exports = app;
